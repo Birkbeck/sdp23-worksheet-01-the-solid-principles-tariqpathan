8f48b179a64c0a85c2230daa44765386b259026e
Tariq Pathan <tariq047@gmail.com>
2023-01-29 17:05:03 +0000

    Extended the BoardOfSpots class into Board - still not sure if I'm answering the question :(

--- 01_single_responsibility/src/Board.java
+++ 01_single_responsibility/src/Board.java
@@ -1,64 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
+public class Board extends BoardOfSpots implements BoardPrinter{
 
-public class Board implements BoardBuilder, BoardPrinter{
-    List<String> spots;
-
-    public Board() {
-        this.spots = new ArrayList<>();
-        for (int i = 0; i < 9; i++) {
-            this.spots.add(String.valueOf(i));
-        }
-    }
     @Override
-    public List<String> firstRow() {
-        List<String> firstRow = new ArrayList<>();
-        firstRow.add(this.spots.get(0));
-        firstRow.add(this.spots.get(1));
-        firstRow.add(this.spots.get(2));
-        return firstRow;
-    }
-    @Override
-    public List<String> secondRow() {
-        List<String> secondRow = new ArrayList<>();
-        secondRow.add(this.spots.get(3));
-        secondRow.add(this.spots.get(4));
-        secondRow.add(this.spots.get(5));
-        return secondRow;
-    }
-    @Override
-    public List<String> thirdRow() {
-        List<String> thirdRow = new ArrayList<>();
-        thirdRow.add(this.spots.get(6));
-        thirdRow.add(this.spots.get(7));
-        thirdRow.add(this.spots.get(8));
-        return thirdRow;
-    }
-
-    // Poor code — can you improve this?
-    //@Override
-    public void display2() {
-        String out =
-                String.join(" | ", this.firstRow())
-                + "\n" +
-                String.join (" | ", this.secondRow())
-                + "\n" +
-                String.join (" | ", this.thirdRow());
-        String o = out.toString();
-        System.out.println(o);
-        System.out.println(out);
-    }
     public void display() {
-        String formattedFirstRow = this.spots.get(0) + " | " + this.spots.get(1) + " | " + this.spots.get(2) + "\n"
-                + this.spots.get(3) + " | " + this.spots.get(4) + " | " + this.spots.get(5) + "\n"
-                + this.spots.get(6) + " | " + this.spots.get(7) + " | " + this.spots.get(8);
-        System.out.println(formattedFirstRow);
+        String out =
+                String.join(" | ", this.firstRow()) + "\n"
+                + String.join (" | ", this.secondRow()) + "\n"
+                + String.join (" | ", this.thirdRow());
+        System.out.println(out);
     }
 
     public static void main(String[] args) {
         Board b = new Board();
-        b.display();
-        b.display2();
         b.display();
     }
 }
---
+++ 01_single_responsibility/src/BoardOfSpots.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardOfSpots implements BoardBuilder{
+    List<String> spots;
+
+    public BoardOfSpots() {
+        this.spots = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            this.spots.add(String.valueOf(i));
+        }
+    }
+
+    @Override
+    public List<String> firstRow() {
+        List<String> firstRow = new ArrayList<>();
+        firstRow.add(this.spots.get(0));
+        firstRow.add(this.spots.get(1));
+        firstRow.add(this.spots.get(2));
+        return firstRow;
+    }
+    @Override
+    public List<String> secondRow() {
+        List<String> secondRow = new ArrayList<>();
+        secondRow.add(this.spots.get(3));
+        secondRow.add(this.spots.get(4));
+        secondRow.add(this.spots.get(5));
+        return secondRow;
+    }
+    @Override
+    public List<String> thirdRow() {
+        List<String> thirdRow = new ArrayList<>();
+        thirdRow.add(this.spots.get(6));
+        thirdRow.add(this.spots.get(7));
+        thirdRow.add(this.spots.get(8));
+        return thirdRow;
+    }
+
+}
